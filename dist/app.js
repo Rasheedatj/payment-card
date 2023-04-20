73c07fc1f0e9621486b32ed868557b6db2cc3ae1
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-04-20 21:22:34 +0100

    added event listeners

--- dist/app.js
+++ dist/app.js
@@ -1,93 +1,47 @@
-let openBtn = document.querySelector('.btn')
-let closeBtn = document.querySelector('.closeModal')
-let modal = document.querySelector('.modal-container')
+let openBtn = document.querySelector('.btn');
+let closeBtn = document.querySelector('.closeModal');
+let modal = document.querySelector('.modal-container');
 
+openBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  modal.style.display = 'flex';
+});
 
-openBtn.addEventListener('click', function(e){
-    modal.style.display = 'flex'
-    e.preventDefault()
-})
+closeBtn.addEventListener('click', function (e) {
+  modal.style.display = 'none';
+});
 
-closeBtn.addEventListener('click', function(e){
-    modal.style.display = 'none'
-})
+// Changing card numbers
 
-// FORM VALIDATION
-let form = document.querySelector('form')
+const cardInput = document.querySelector('#card-number');
+const mainCardNumber = document.querySelector('.card-number');
 
-function validation(){   
-// variables
-let cardNumber = document.querySelector('#card-input').value
-let numberText = document.querySelector('.card-error-message')
-let cardBorder = document.querySelector('#card-input')
+const nameInput = document.querySelector('#name');
+const mainCardName = document.querySelector('.name');
 
-// pattern
-const numPattern = /^[0-9]{3}[0-9]{3}[0-9]{3}[0-9]{3}/
+const cvcInput = document.querySelector('#cvc-input');
+const mainCardCvc = document.querySelector('#cvc');
 
-    if(cardNumber.match(numPattern)){
-        form.classList.add('valid')
-        form.classList.remove('invalid')
-        numberText.innerText = ''
-        cardBorder.style.border = '1px solid hsl(270, 3%, 87%)'
-    }else{
-        form.classList.remove('valid')
-        form.classList.add('invalid')
-        numberText.innerText = 'Wrong format. numbers only'
-        cardBorder.style.border = '1px solid red'
-    }  
-}
+const monthInput = document.querySelector('#month');
+const mainCardMonth = document.querySelector('.month');
 
-function dateVaidation(){
-    let blankMessage = document.querySelector('.date-error-message')
-    let expiryDate = document.querySelector('#date-input').value
-    let expireBorder = document.querySelector('#date-input')
-    const pattern = /^[0-9]{1}$/
-    if(expiryDate.match(pattern)){
-        form.classList.add('MMvalid')
-        form.classList.remove('MMinvalid')
-        blankMessage.innerText = ''
-        expireBorder.style.border = '1px solid hsl(270, 3%, 87%)'
-    }else{
-        form.classList.remove('MMvalid')
-        form.classList.add('MMinvalid')
-        blankMessage.innerText =`can't be blank`
-        expireBorder.style.border = '1px solid red'
-    }
-}
+const yearInput = document.querySelector('#year');
+const mainCardYear = document.querySelector('.year');
 
-function yearVaidation(){
-    let blankMessage = document.querySelector('.date-error-message')
-    let expiryDate = document.querySelector('#year-input').value
-    let expireBorder = document.querySelector('#year-input')
-    const pattern = /^[0-9]{1}$/
-    if(expiryDate.match(pattern)){
-        form.classList.add('MMvalid')
-        form.classList.remove('MMinvalid')
-        blankMessage.innerText = ''
-        expireBorder.style.border = '1px solid hsl(270, 3%, 87%)'
-    }else if(expiryDate === ''){
-        form.classList.remove('MMvalid')
-        form.classList.add('MMinvalid')
-        blankMessage.innerText = `can't be blank`
-        expireBorder.style.border = '1px solid red'
-    }
-}
+cardInput.addEventListener('input', () => {
+  mainCardNumber.innerHTML = cardInput.value;
+});
 
+nameInput.addEventListener('input', () => {
+  mainCardName.innerHTML = nameInput.value;
+});
 
-function cvcValidation(){
-    let blankMessage = document.querySelector('.cvc-error-message')
-    let cvc = document.querySelector('#cvc-input').value
-    let cvcBorder =document.querySelector('#cvc-input')
-    const pattern = /^[0-9]{2}$/
-    if(cvc.match(pattern)){
-        form.classList.add('cvcvalid')
-        form.classList.remove('cvcinvalid')
-        blankMessage.innerText = ''
-        cvcBorder.style.border = '1px solid hsl(270, 3%, 87%)'
-    }else {
-        form.classList.remove('cvcvalid')
-        form.classList.add('cvcinvalid')
-        blankMessage.innerText = `can't be blank`
-        cvcBorder.style.border = '1px solid red'
-    }
-}
+cvcInput.addEventListener('input', () => {
+  mainCardCvc.innerHTML = cvcInput.value;
+});
+monthInput.addEventListener('input', () => {
+  mainCardMonth.innerHTML = monthInput.value;
+});
+yearInput.addEventListener('input', () => {
+  mainCardYear.innerHTML = yearInput.value;
+});
